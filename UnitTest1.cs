a55120b765364d1eb73b63a1db91c98795ed18e0
Maximilian.gav <zaluekos@gmail.com>
2024-08-22 21:28:02 +0300

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,19 @@
+namespace GoogleCloud01;
+
+using OpenQA.Selenium;
+using static Gogli1Test;
+
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
